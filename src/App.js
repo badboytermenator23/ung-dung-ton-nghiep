ab92926c7b3d500d1fe81e0e69a84afc6d1c8f25
badboytermenator23 <dziopaabends@hotmail.com>
2025-08-15 06:09:39 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ứng dụng Tổn Nghiệp</h1>
+        <p>Tìm kiếm và quản lý việc làm theo kỹ năng và sở thích của bạn!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
